a8237f32e809a0cbfcbc4b2535c8aa84428fd87b
Muhamma Farhan Fadlillah <ffadlillah18@gmail.com>
2023-09-27 09:28:15 +0700

    Jurnal01

---
+++ tugas.java
@@ -0,0 +1,7 @@
+public class tugas {
+    public static void main(String[] args) {
+        System.out.println("Nama: Muhammad Farhan Fadlillah");
+        System.out.println("NIM: 607062300036");
+        System.out.println("Kelas: D3IF-47-04");
+    }
+}
